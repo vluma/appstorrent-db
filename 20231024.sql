1cc506c4594c147cad05bc566d82da7b54e15a3a
马路伟 <maluwei0216@gmail.com>
2023-10-24 09:56:38 +0800

    Auto commit: update data

---
+++ 20231024.sql
@@ -0,0 +1,151 @@
+
+      INSERT INTO app (name, version, description, description_local, icon, image, detail_url, file_size, category, published_date, architecture, source)
+      VALUES ('Storyteller', '1.1.13', '
+ИгрыГоловоломки
+', '
+游戏益智
+', 'https://appstorrent.ru/uploads/posts/2023-10/13ds.webp', 'https://appstorrent.ru/uploads/posts/2023-10/999909f0sd9f0sdf99ds009ds0.webp', 'https://appstorrent.ru/2609-storyteller.html', '846 MB', 2.0, '2023-10-24', 'ARM, x86 (64-bit)', 'appstorrent.ru')
+  ;
+
+    UPDATE app
+    SET
+        name = 'Storyteller',
+        bg_image = 'https://appstorrent.ru/uploads/posts/2023-10/990r99e90er90re90er90re09re09er09.webp',
+        icon = 'https://appstorrent.ru/uploads/posts/2023-10/13ds.webp',
+        version = '1.1.13',
+        description = '
+ИгрыГоловоломки
+',
+        description_local = '
+游戏益智
+',
+        detail_url = 'https://appstorrent.ru/2609-storyteller.html',
+        file_size = '846 MB',
+        developer = 'Daniel Benmergui',
+        activation = 'Не требуется',
+        compatibility = '',
+        architecture = 'ARMx86 (64-bit)',
+        published_date = '2023/10/24',
+        releaseDate = '',
+        note = '',
+        views = '798',
+        link = 'http://hidereferrer.net/?',
+        detail_version = '1.1.13',
+        banner = '["https://appstorrent.ru/uploads/posts/2023-10/ss_cb1b6c6addb69a3182c8ed89ca5f30cb25b7eefd_1920x1080.webp","https://appstorrent.ru/uploads/posts/2023-10/1696263629_ss_5f99d37b23fc3d4469c40dfc78a69cd3a51d4f26_1920x1080.webp","https://appstorrent.ru/uploads/posts/2023-10/ss_9f7f204fae84954897fcf76b951f0f32dde65f5d_1920x1080.webp","https://appstorrent.ru/uploads/posts/2023-10/ss_abb2a53ef96ab74a2d1c8d4bef88944d4f767303_1920x1080.webp"]'
+    WHERE
+        id = 2491.0
+    ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Русский', 'https://appstorrent.ru/info-lang/%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Английский', 'https://appstorrent.ru/info-lang/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Немецкий', 'https://appstorrent.ru/info-lang/%D0%BD%D0%B5%D0%BC%D0%B5%D1%86%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Арабский', 'https://appstorrent.ru/info-lang/%D0%B0%D1%80%D0%B0%D0%B1%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Испанский', 'https://appstorrent.ru/info-lang/%D0%B8%D1%81%D0%BF%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Итальянский', 'https://appstorrent.ru/info-lang/%D0%B8%D1%82%D0%B0%D0%BB%D1%8C%D1%8F%D0%BD%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Китайский (трад.)', 'https://appstorrent.ru/info-lang/%D0%BA%D0%B8%D1%82%D0%B0%D0%B9%D1%81%D0%BA%D0%B8%D0%B9%20%28%D1%82%D1%80%D0%B0%D0%B4.%29/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Китайский (упр.)', 'https://appstorrent.ru/info-lang/%D0%BA%D0%B8%D1%82%D0%B0%D0%B9%D1%81%D0%BA%D0%B8%D0%B9%20%28%D1%83%D0%BF%D1%80.%29/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Корейский', 'https://appstorrent.ru/info-lang/%D0%BA%D0%BE%D1%80%D0%B5%D0%B9%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Польский', 'https://appstorrent.ru/info-lang/%D0%BF%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Португальский', 'https://appstorrent.ru/info-lang/%D0%BF%D0%BE%D1%80%D1%82%D1%83%D0%B3%D0%B0%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Тайский', 'https://appstorrent.ru/info-lang/%D1%82%D0%B0%D0%B9%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Турецкий', 'https://appstorrent.ru/info-lang/%D1%82%D1%83%D1%80%D0%B5%D1%86%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Французский', 'https://appstorrent.ru/info-lang/%D1%84%D1%80%D0%B0%D0%BD%D1%86%D1%83%D0%B7%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Шведский', 'https://appstorrent.ru/info-lang/%D1%88%D0%B2%D0%B5%D0%B4%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Японский', 'https://appstorrent.ru/info-lang/%D1%8F%D0%BF%D0%BE%D0%BD%D1%81%D0%BA%D0%B8%D0%B9/', 1.0)
+          ;
+
+              INSERT INTO languages (app_id, name, link, type)
+              VALUES (2491.0, 'Музыкальное сопровождение', 'https://appstorrent.ru/info-lang-voice/%D0%BC%D1%83%D0%B7%D1%8B%D0%BA%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5%20%D1%81%D0%BE%D0%BF%D1%80%D0%BE%D0%B2%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5/', 2.0)
+          ;
+
+  INSERT INTO categories (app_id, content, href, content_local)
+  VALUES (2491.0, 'Игры', 'https://appstorrent.ru/games/', NULL)
+;
+
+  INSERT INTO categories (app_id, content, href, content_local)
+  VALUES (2491.0, 'Головоломки', 'https://appstorrent.ru/games/puzzle/', NULL)
+;
+
+      INSERT INTO tabs (app_id, title, content, content_local)
+      VALUES (2491.0, 'Описание', '
+<p>Storyteller – это сочинение сказок на языке картинок. Цель каждого уровня: придумать сказку с заданным названием, выбирая обстановку и персонажей так, чтобы их взаимные реакции складывались в историю с определенным концом.</p>
+<p><br></p>
+<p>Мультипликационный дизайн и забавная анимация Storyteller позволяют экспериментировать с самыми разнообразными сюжетами: просто перетаскивайте персонажей – и смотрите, как сказывается сказка!</p>
+<p><br></p>
+<p>Играйте целой коллекцией сюжетов и персонажей, героев и злодеев, вампиров и драконов, неверных и мстительных супругов в калейдоскопе любви, лжи, безумия и других бурных чувств! Заполняйте пустые рамки секретами и страстями, убивайте монстров или пугайте ими людей, повелевайте силами любви и коварства. Все обожают сказки, но теперь их творцом станете вы.</p>
+', '
+<p>说书人就是用图画语言来写童话故事。 每个级别的目标是创建一个具有给定标题的童话故事，选择背景和人物，以便他们的相互反应形成一个具有特定结局的故事。</p>
+<p><br></p>
+<p>Storyteller 的卡通设计和有趣的动画让您可以尝试各种各样的故事 - 只需拖放角色并观看故事展开！</p>
+<p><br></p>
+<p>在充满爱、谎言、疯狂和其他动荡情感的万花筒中，体验一整套故事和人物、英雄和恶棍、吸血鬼和龙、不忠和复仇的配偶！ 用秘密和激情填充空的框架，杀死怪物或用它们吓唬人，指挥爱和欺骗的力量。 每个人都喜欢童话，但现在你将成为童话的创造者。</p>
+')
+  ;
+
+      INSERT INTO tabs (app_id, title, content, content_local)
+      VALUES (2491.0, 'Мин. требования', '
+<p>ОС: OS X Yosemite<br>Процессор: Intel Core i5-3470S<br>Оперативная память: 8 GB ОЗУ<br>Видеокарта: NVIDIA GeForce GT 650M, 512 MB | Intel UHD 630<br>Место на диске: 1 GB<br>Дополнительно: Details: Low 1080p @ 60 FPS</p>
+', '
+<p>操作系统：OS X 优胜美地<br>处理器：英特尔酷睿 i5-3470S<br>内存：8 GB 内存<br>显卡：NVIDIA GeForce GT 650M，512 MB | 英特尔超核芯 630<br>磁盘空间：1 GB<br>另外：详细信息：低 1080p @ 60 FPS</p>
+')
+  ;
+
+  INSERT OR REPLACE INTO download_info (app_id, content, content_local)
+  VALUES (2491.0, '<p>Запустите скачанный образ и перетащите приложение в ярлык папки Программы (Applications).<br>После завершения копирования, приложение можно запустить через Launchpad.</p>
+<p><br></p>
+<p>Если игра не запускается введите в терминале:</p>
+<p><span style="color:rgb(230,126,35);">xattr -cr /Applications/"Storyteller.app" &amp;&amp; codesign --force --deep --sign - /Applications/"Storyteller.app"</span></p>', '<p>启动下载的图像并将应用程序拖到“应用程序”文件夹快捷方式中。<br>复制完成后，可以通过 Launchpad 启动应用程序。</p>
+<p><br></p>
+<p>如果游戏没有开始，请在终端输入：</p>
+<p><span style="color:rgb(230,126,35);">xattr -cr /Applications/"Storyteller.app" &amp;&amp; codesign --force --deep --sign - /Applications/"Storyteller.app"</span></p>')
+;
+
+      INSERT OR REPLACE INTO download_links (app_id, title, href, title_local, version)
+      VALUES (2491.0, 'Прямая ссылка на скачивание', 'https://vk.com/s/v1/doc/TBkkx-M7_mrxDfCVWG0kzTlHm_FpFJkv63ehZX1lZxb2bu45kbg', '直接链接下载安装程序', '1.1.13')
+  ;
